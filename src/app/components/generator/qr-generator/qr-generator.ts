420e866ff8220d83c531c9000a4a2c60667cb11f
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-06 20:36:37 +0800

    Initialize generator components html

---
+++ src/app/components/generator/qr-generator/qr-generator.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-generator',
+  imports: [],
+  templateUrl: './qr-generator.html',
+  styleUrl: './qr-generator.scss',
+})
+export class QrGenerator {
+
+}
