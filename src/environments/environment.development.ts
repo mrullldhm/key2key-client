9fa3a707e3885499eef6b929be4c72f69a80fe88
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-09 17:45:24 +0800

    Initialize angular environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5500/api/v1', // same structure as environment.ts
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5500/api/v1' // Change this to your Express server port
+};
