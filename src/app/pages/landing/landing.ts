0ef52748a20d838a33ee936ea1b8949224016d28
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-09 17:44:49 +0800

    Add router link for CTA

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
-export class Landing {
-
-}
+export class Landing {}
