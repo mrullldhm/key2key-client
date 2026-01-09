bc60ad0b765ed2532337965679f2a95c119e8b8c
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-09 20:58:05 +0800

    Add logout function

--- src/app/components/navbar/vault-navbar/vault-navbar.ts
+++ src/app/components/navbar/vault-navbar/vault-navbar.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth } from '../../../services/auth';
+import { Router } from '@angular/router';
+import { VaultState } from '../../../services/vault-state';
 
 @Component({
   selector: 'app-vault-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './vault-navbar.html',
   styleUrl: './vault-navbar.scss',
 })
 export class VaultNavbar {
+  private authService = inject(Auth);
+  private vaultState = inject(VaultState);
+  private router = inject(Router);
 
+  onLogout() {
+    // 1. Clear the sensitive keys from Angular Signals (RAM)
+    this.vaultState.clearVault();
+
+    // 2. Clear the JWT
+    this.authService.logout();
+
+    // 3. Go back to landing
+    this.router.navigate(['/']);
+  }
 }
