16b1d87429d45202aa8694f18a4cceedf8c7da12
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-10 13:36:40 +0800

    Use footer component

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-landing',
-  imports: [RouterLink],
+  imports: [RouterLink, Footer],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
