b7315a612c321a12170a7c592854d01163696439
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-10 13:37:23 +0800

    Create a blank access component

---
+++ src/app/components/access/access.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access',
+  imports: [],
+  templateUrl: './access.html',
+  styleUrl: './access.scss',
+})
+export class Access {
+
+}
