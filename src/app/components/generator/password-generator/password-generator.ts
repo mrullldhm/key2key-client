ecdec2829feb0d3ebfce449576fcfbfab4174772
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-10 14:44:19 +0800

    Setup function for password generator

--- src/app/components/generator/password-generator/password-generator.ts
+++ src/app/components/generator/password-generator/password-generator.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-generator',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './password-generator.html',
   styleUrl: './password-generator.scss',
 })
 export class PasswordGenerator {
+  password = '';
 
+  length = 16;
+  includeUppercase = true;
+  includeLowercase = true;
+  includeNumbers = true;
+  includeSymbols = true;
+
+  generatePassword() {
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let chars = '';
+
+    if (this.includeUppercase) chars += upper;
+    if (this.includeLowercase) chars += lower;
+    if (this.includeNumbers) chars += numbers;
+    if (this.includeSymbols) chars += symbols;
+
+    if (!chars) {
+      this.password = '';
+      return;
+    }
+
+    let result = '';
+    for (let i = 0; i < this.length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      result += chars[randomIndex];
+    }
+
+    this.password = result;
+  }
+
+  copyPassword() {
+    if (!this.password) return;
+    navigator.clipboard.writeText(this.password);
+  }
 }
