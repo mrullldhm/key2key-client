fa42bb36173814908e965a69ee51e1ec2d5bd90d
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-11 09:54:09 +0800

    Add HTTP Interceptor

---
+++ src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('k2k_token');
+
+  // If token exists, clone the request and add the Authorization header
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+
+  // If no token, just pass the original request
+  return next(req);
+};
