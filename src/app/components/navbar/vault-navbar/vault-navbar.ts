07a615c22e492e7c2096677e68e24c9f9856754a
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-01-11 17:58:22 +0800

    Initialize search funtion for search box

--- src/app/components/navbar/vault-navbar/vault-navbar.ts
+++ src/app/components/navbar/vault-navbar/vault-navbar.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { VaultState } from '../../../services/vault-state';
 
 @Component({
   selector: 'app-vault-navbar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vault-navbar.html',
   styleUrl: './vault-navbar.scss',
 })
 export class VaultNavbar {
+  public vaultState = inject(VaultState); // Inject the state
   @Output() navigate = new EventEmitter<'dashboard' | 'generator'>();
 
   goDashboard() {
